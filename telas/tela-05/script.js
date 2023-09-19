99637435871b6e2823a0169647252395ef54824c
luis-mendes-95 <luis.mendes.95@hotmail.com>
2023-09-19 11:28:46 -0300

    add timeout for rendering message balloons

--- telas/tela-05/script.js
+++ telas/tela-05/script.js
@@ -11,42 +11,55 @@ const mudar_seta_info = () => {
     setas_informacoes.src = "./assets/setas_informacoes/informacao_01.png";
     setas_informacoes.style.top = "20%";
     setas_informacoes.style.left = "20%";
-    content.appendChild(setas_informacoes)
+    setTimeout(() => {
+      content.appendChild(setas_informacoes)
+    }, 300);
+
   }
   if (informacao_num === 2) {
     content.removeChild(setas_informacoes)
     setas_informacoes.src = "./assets/setas_informacoes/informacao_02.png";
     setas_informacoes.style.top = "30%";
     setas_informacoes.style.left = "40%";
-    content.appendChild(setas_informacoes)
+    setTimeout(() => {
+      content.appendChild(setas_informacoes)
+    }, 300);
   }
   if (informacao_num === 3) {
     content.removeChild(setas_informacoes)
     setas_informacoes.src = "./assets/setas_informacoes/informacao_03.png";
     setas_informacoes.style.top = "40%";
     setas_informacoes.style.left = "28%";
-    content.appendChild(setas_informacoes)
+    setTimeout(() => {
+      content.appendChild(setas_informacoes)
+    }, 300);
   }
   if (informacao_num === 4) {
     content.removeChild(setas_informacoes)
     setas_informacoes.src = "./assets/setas_informacoes/informacao_04.png";
     setas_informacoes.style.top = "48%";
     setas_informacoes.style.left = "39%";
-    content.appendChild(setas_informacoes)
+    setTimeout(() => {
+      content.appendChild(setas_informacoes)
+    }, 300);
   }
   if (informacao_num === 5) {
     content.removeChild(setas_informacoes)
     setas_informacoes.src = "./assets/setas_informacoes/informacao_05.png";
     setas_informacoes.style.top = "42%";
     setas_informacoes.style.left = "52%";
-    content.appendChild(setas_informacoes)
+    setTimeout(() => {
+      content.appendChild(setas_informacoes)
+    }, 300);
   }
   if (informacao_num === 6) {
     content.removeChild(setas_informacoes)
     setas_informacoes.src = "./assets/setas_informacoes/informacao_06.png";
     setas_informacoes.style.top = "52%";
     setas_informacoes.style.left = "67%";
-    content.appendChild(setas_informacoes)
+    setTimeout(() => {
+      content.appendChild(setas_informacoes)
+    }, 300);
   }
 };
 
